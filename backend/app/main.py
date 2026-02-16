7cd99e0f2ab5313e65e500821ef47007456257e3
Anas-Ah25 <s-anas.abdelsalam@zewailcity.edu.eg>
2026-02-16 07:43:25 +0200

    fix/candidate-video-interview fixes #37

--- backend/app/main.py
+++ backend/app/main.py
@@ -40,7 +40,7 @@ app.add_middleware(
 
 app.include_router(api_v1_router, prefix=settings.API_V1_STR)
 
-# Mount static files
+# mount  static files where we will download the videos 
 static_dir = os.path.join(os.getcwd(), "static")
 os.makedirs(static_dir, exist_ok=True)
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
